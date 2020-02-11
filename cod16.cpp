47c61e43984d80940d5319181a8b18ba57416d89
Nemolite <g25092011@mail.ru>
2020-02-11 16:51:49 +0300

    cod17

--- cod16.cpp
+++ cod16.cpp
@@ -8,12 +8,27 @@ struct DList {
 
 int main(){ 
   int n;
-  DList *ukas;
+  DList *ukas,*ukas1,*ukas2;
   ukas = new DList;
+  ukas1 = new DList;
+  ukas2 = new DList;
   cout<<ukas<<endl;
+  cout<<ukas1<<endl;
+  cout<<ukas2<<endl;
+    cout<<"---------------"<<endl;
   ukas->m = 20;
-  ukas->p = ukas;
+  ukas->p = ukas1;
   cout<<ukas->m<<endl;
   cout<<ukas->p<<endl;
+ 
+  ukas1->m = 100;
+  ukas1->p = ukas2;
+  cout<<ukas1->m<<endl;
+  cout<<ukas1->p<<endl;
+  
+  ukas2->m = 400;
+  ukas2->p = ukas;
+  cout<<ukas2->m<<endl;
+  cout<<ukas2->p<<endl;
   return 0;
 }
---
+++ cod17.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){ 
+enum color {
+	red,
+	green,
+	blue,
+	asd,
+	asdfasf=3000
+};  
+
+color bus=asdfasf;
+
+cout<<bus<<endl;
+  return 0;
+}
