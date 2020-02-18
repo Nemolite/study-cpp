3a59c14a57ece0042da7576cd4f3c0848c544fae
Nemolite <g25092011@mail.ru>
2020-02-18 17:51:34 +0300

    cppv18

---
+++ cod18.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){ 
+  int n;
+  n=1;
+  int m;
+  m=n>2?22:33;
+  cout<<m<<endl;
+  if (n>2)
+  m = 22;
+  else
+  m = 33;
+ cout<<m<<endl;
+  
+
+
+  return 0;
+}
